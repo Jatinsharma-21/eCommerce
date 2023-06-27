e2ce278ccdee1220e2f42eb99d1577e570c6b513
Jatin Sharma <jatinsharma21092002@gmail.com>
2023-06-27 13:36:07 +0530

    updated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ dotenv.config();
 //databse config
 connectDB();
 
-// esmodule fix
+// esmodule fix 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
